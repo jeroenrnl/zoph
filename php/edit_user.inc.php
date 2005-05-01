94def0ae30fb9804154ff0cdabee7aaf6bd241a1
jeroenrnl <jeroen@zoph.org>
2005-05-01 11:55:29 +0000

    Small layout fix

--- php/edit_user.inc.php
+++ php/edit_user.inc.php
@@ -24,7 +24,6 @@
 <?php
     }
 ?>
-
 <?php echo translate("user name") ?>
           </td>
           <td class="field"><?php echo create_text_input("user_name", $this_user->get("user_name"), 16, 16) ?></td>
@@ -32,54 +31,54 @@
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("person") ?></td>
-          <td colspan="2">
+          <td colspan="2" class="field">
 <?php echo create_smart_pulldown("person_id", $action == "insert" ? "1" : $this_user->get("person_id"), get_people_select_array()) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("password") ?></td>
-          <td><input type="password" name="password" value="" size="16" maxlength="32"></td>
+          <td class="field"><input type="password" name="password" value="" size="16" maxlength="32"></td>
           <td class="inputhint"><?php echo sprintf(translate("%s chars max"), "32") ?></td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("class") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php echo create_pulldown("user_class", $this_user->get("user_class"), array("1" => translate("User",0), "0" => translate("Admin",0)) ) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("can browse people") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php echo create_pulldown("browse_people", $this_user->get("browse_people"), array("0" => translate("No",0), "1" => translate("Yes",0)) ) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("can browse places") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php echo create_pulldown("browse_places", $this_user->get("browse_places"), array("0" => translate("No",0), "1" => translate("Yes",0)) ) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("can view details of people") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php echo create_pulldown("detailed_people", $this_user->get("detailed_people"), array("0" => translate("No",0), "1" => translate("Yes",0)) ) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("can view details of places") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php echo create_pulldown("detailed_places", $this_user->get("detailed_places"), array("0" => translate("No",0), "1" => translate("Yes",0)) ) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("can import") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php echo create_pulldown("import", $this_user->get("import"), array("0" => translate("No",0), "1" => translate("Yes",0)) ) ?>
           </td>
         </tr>
         <tr>
           <td class="fieldtitle"><?php echo translate("lightbox album") ?></td>
-          <td colspan="2">
+          <td class="field" colspan="2">
 <?php
     $lightbox_array = get_albums_select_array();
     $lightbox_array["null"] = "[none]";
