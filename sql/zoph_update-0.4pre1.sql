a8b01888bd5676b2ee674c15742deaaf7f46adcc
jeroenrnl <jeroen@zoph.org>
2005-07-28 16:42:52 +0000

    Changed license to GPL

--- sql/zoph_update-0.4pre1.sql
+++ sql/zoph_update-0.4pre1.sql
@@ -1,6 +1,21 @@
 #
 # Zoph 0.3.3 -> 0.4pre1 update
 #
+# This file is part of Zoph.
+#
+# Zoph is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Zoph is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Zoph; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 #
 # rename tables using a zoph_ prefix to avoid naming conflicts.
