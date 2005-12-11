1291deffc958d5f4e6b69e2e9e928f28f65278c1
jeroenrnl <jeroen@zoph.org>
2005-12-11 20:36:31 +0000

    Updated HTML/CSS to better semantics. Will need some cleanout later.

--- php/css.php
+++ php/css.php
@@ -39,13 +39,19 @@ body	{
 	font-size: medium;
 	color: <?php echo $TEXT_COLOR ?>;
 	background: <?php echo $PAGE_BG_COLOR ?>;
+	width: <?php echo DEFAULT_TABLE_WIDTH ?>px;
+	border: none;
+	margin-left: auto; /* To center the page */
+	margin-right: auto;
+	padding: 0px;
+	border-collapse: collapse;
 	}
 
 /* Links */
 
 a	{
 	color: <?php echo $LINK_COLOR ?>;
-	background: transparant;
+	background: transparent;
 	}
 
 /* Images that are links */
@@ -54,19 +60,30 @@ a IMG	{
 	border: none;
 	}
 
-/* Page title */
 h1	{
+	background: <?php echo $TITLE_BG_COLOR ?>;
+	color: <?php echo $TITLE_FONT_COLOR ?>;
+	border: 1px solid <?php echo $TABLE_BORDER_COLOR ?>; 
 	text-align: left;
+	width: <?php echo (DEFAULT_TABLE_WIDTH-8-2) ?>px;
+	clear: left;
 	font-size: x-large;
-	margin: 0px;
 	font-weight: bold;
+	display: block;
+	padding: 2px 4px 2px 4px;
+	margin: 0;
 	}
 
 /* Secondary title such as album title */
 h2	{
 	text-align: left;
 	font-size: large;
-	margin: 0px;
+	margin: 0;
+	margin-bottom: 10px;
+	}
+
+h2.logon {
+	margin-bottom: 30px;
 	}
 
 /* Level 3 title */
@@ -88,78 +105,67 @@ form	{
 	margin: 0 0 0 0;
 	}
 
-/* The table that takes care of the border around the page */
+/* Takes care of the border around the page */
 
-table.page {
-	background: <?php echo $TABLE_BORDER_COLOR ?>; 
-	width: <?php echo DEFAULT_TABLE_WIDTH ?>px;
-	border: none;
-	margin-left: auto; /* To center the page */
-	margin-right: auto;
-	padding: 0px;
-	border-collapse: collapse;
-	}
 	
-table.page > td, table.page > th {
-	margin: 0px;
-	padding: 1px;
-	}
-
 /* Menubar */
-.menu	{
-	background: <?php echo $PAGE_BG_COLOR ?>;
-	}
 
-#menu ul	{
+ul.menu	{
+	background: <?php echo $PAGE_BG_COLOR ?>;
 	margin-left: 4px;
 	padding: 0;
 	display: inline;
+	width: <?php echo (DEFAULT_TABLE_WIDTH-8) ?>px;
 	}
 
-#menu ul li 	{
+ul.menu li 	{
 	padding: 1px;
 	padding-top: 3px;
-	margin: -1px;
+	margin: 1px;
+	border: none;
 	text-align: center;
 	list-style: none;
 	display: inline;
-        background: <?php echo $TAB_BG_COLOR ?>;
+	background: <?php echo $TAB_BG_COLOR ?>;
         color: <?php echo $TAB_FONT_COLOR ?>;
 	font-size: small;
 	}
 
-#menu ul li.selected {
+ul.menu li.selected {
 	background: <?php echo $SELECTED_TAB_BG_COLOR ?>;
 	color: <?php echo $SELECTED_TAB_FONT_COLOR ?>;
 	}
 								
 /* since the A element does not inherit font colors from it's parents, we set it explicetly here. Also underlining is removed from links in menu, unless it is hovered */
 
-#menu ul li > a { 
+ul.menu li a { 
 	color: <?php echo $TAB_FONT_COLOR ?>; 
 	text-decoration: none; 
 	}
 	
-#menu ul li > a:hover { text-decoration: underline; }
-#menu ul li.selected > a { color: <?php echo $SELECTED_TAB_FONT_COLOR ?>; }
+ul.menu li > a:hover { text-decoration: underline; }
+ul.menu li.selected > a { color: <?php echo $SELECTED_TAB_FONT_COLOR ?>; }
 
 /* The breadcrumb line at the top of the page */
 
-#breadcrumb {
+div.breadcrumb {
 	background: <?php echo $BREADCRUMB_BG_COLOR ?>;
 	border: 1px solid <?php echo $TABLE_BORDER_COLOR ?>;
 	color: <?php echo $TEXT_COLOR ?>;
 	font-size: small;
-	}
-
-#breadcrumb ul {
-	margin: 0;
-	padding: 0;
 	float: left;
-	border: none;
+	margin: 0;
+	padding: 2px;
+	clear: left;
+	width: <?php echo (DEFAULT_TABLE_WIDTH-2-4) ?>px;
 	} 
 
-#breadcrumb ul li {
+div.breadcrumb ul {
+	margin: 0;
+	padding: 0;
+	}
+	
+div.breadcrumb li {
 	margin-left: 1px;
 	padding-left: 2px;
 	border: none;
@@ -167,79 +173,109 @@ table.page > td, table.page > th {
 	display: inline;
 	}
 
-#breadcrumb ul li:before {
+div.breadcrumb li:before {
 	content: "\0020 \0020 \0020 \00BB \0020";
 	}
 	
-#breadcrumb ul li.first:before {
+div.breadcrumb li.first:before {
 	content: " ";
 	}
 
-#breadcrumb ul li.firstdots:before {
+div.breadcrumb li.firstdots:before {
 	content: "... \00BB \0020 ";
 	}
 
 /* Main page */
-.main, table.info	{
+.main, .info, .letter	{
 	background: <?php echo $TABLE_BG_COLOR ?>;
 	font-size: medium;
-	width: 100%;
+	width: <?php echo (DEFAULT_TABLE_WIDTH-20-2) ?>px;
 	border-spacing: 0px;
 	padding: 10px;
+	border: 1px solid <?php echo $TABLE_BORDER_COLOR ?>;
+	clear: both;
 	}
 
-table.main > td, table.main > th, table.info > td, table.info > th	{
+p.main, p.info {
 	padding: 4px;
 	}
 
 table.info > col { width: 50%; }
 
-/* Some pages have a table nested inside the "main" table */
-table.content	{
-	margin-left: auto; /* To center the page */
-	margin-right: auto;
+/* The short introduction on zoph.php */
+.intro	{
+        padding: 5px;
+	text-align: left;
 	}
 
+div.intro {
+	float: right;
+	clear: right;
+	width: <?php echo DEFAULT_TABLE_WIDTH-THUMB_SIZE-50 ?>px;
+	}
+
+div.intro ul {
+	margin: 5px;
+	padding-bottom: 0px;
+	}
+	
+
+p.intro	{
+	clear: both;
+	margin-top: 2px;
+	margin-bottom: 2px;
+	}
+	
 /* ratings and reports are used on the reports page */
 
 table.ratings	{
 	margin-left: auto; /* To center the page */
 	margin-right: auto;
+	padding: 10px 5px 20px 5px;
 	}
 
+table.reports {
+	width: 50%;
+	float: left;
+	padding: 10px 5px 20px 5px;
+}
+	
+/* This is the bar that shows the number of photos for each rating */
 div.ratings   {
        float: left;
        background: <?php echo $BREADCRUMB_BG_COLOR ?>;
        }
 
-table.reports {
+table.places {
 	width: 100%;
 	}
 
-td.reports {
-	  text-align: left;
-	  vertical-align: top;
-	  width: 50%;
-	  }
-			  
-/* Main titlebar */
-.titlebar	{
-	background: <?php echo $TITLE_BG_COLOR ?>;
-	color: <?php echo $TITLE_FONT_COLOR ?>;
-	text-align: left;
-	width: 100%;
+table.places td	{
+	padding-left: 5px;
+	padding-right: 5px;
 	}
-
 
 /* Links that appear on the right hand side of the title bar or page */
 
-.actionlink {
+span.actionlink {
 	margin: 1px;
 	text-align: right;
 	vertical-align: top;
 	font-size: x-small;
-	display: block;
 	float: right;
+	}
+	
+td.actionlink {
+	text-align: right;
+	font-size: x-small;
+	}
+
+.actionlink:before, div.letter:before {
+	content: "[ ";
+	}
+
+.actionlink:after, div.letter:after {
+	content: " ]";
 	}
 
 /* Text next to 'remove' tickbox */
@@ -300,56 +336,73 @@ table.colors > td, table.colors > th	{
 
 /* Tables for the permissions */
 
-table.permissions	{
+table#permissions	{
 	background: <?php echo $TABLE_BG_COLOR ?>;
-	width: 100%;
+	width: 90%;
 	margin-left: auto; /* To center the page */
 	margin-right: auto;
 	border-collapse: collapse;
 	font-size: medium;
 	}	
 
-table.permissions > td, table.permissions > th	{
+table#permissions td, table#permissions th	{
 	background: <?php echo $TABLE_BG_COLOR ?>;
 	font-size: medium;
 	}
 
-table.permissions > col {
+table#permissions col {
 	text-align: center; 
 	padding: 0px;
 	}
 
-table.permissions > col.col1 { 
+table#permissions > col.col1 { 
 	padding-left: 15px; 
 	text-align: left; 
 	width: 5%;
 	}
 	
-table.permissions > col.col2 { width: 55%; text-align: left; }
-table.permissions > col.col3 { width: 20%; text-align: center; }
-table.permissions > col.col4 { width: 20%; text-align: center;}
+table#permissions > col.col2 { width: 55%; text-align: left; }
+table#permissions > col.col3 { width: 20%; text-align: center; }
+table#permissions > col.col4 { width: 20%; text-align: center;}
 
-td.permremove	{
+table#permissions td.permremove	{
 	padding-top: 3px;
 	padding-bottom: 0px;
 	font-size: x-small;
-	align: left;
+	text-align: left;
 	vertical-align: bottom;
 	}
 
 /* Previous and next links above a photo */
-.prev, .next { font-size: small; }
-.prev	{ text-align: left; }
-.next	{ text-align: right; }
-td.prev, td.next { width: 20%; }
-
-/* Page links */
-.pagelink	{
+div#prev, div#next, div#pagelink, div#photohdr	{
+	margin-bottom: 2px;
+	margin-top: 30px;
 	font-size: small;
-	text-align: center;
+	float: left;
+	}
+
+div#prev	{ 
+	width: 20%;
+	float: left; 
+	text-align: left 
 	}
 	
-.currentpage	{
+div#next	{
+	width: 20%;
+	float: right;
+	text-align: right;
+	clear: right;
+	}
+
+
+/* Page links */
+div#pagelink,
+div#photohdr {
+	text-align: center;
+	width: 60%;
+	}
+	
+#currentpage	{
 	color: red;
 	font-weight: bold;
 	}
@@ -361,30 +414,61 @@ td.prev, td.next { width: 20%; }
 	display: block; /* needed to make the arrows exactly connect */
 	}
 
-/* Header showing name, size in pixels and size in bytes above a photo */
+/* Form on top of each photopage to determine sortorder, asc or desc and
+   number of photos displayes */
+
+
+div#sortorder {
+	float: left;
+	margin-bottom: 15px;
+	}
+
+div#updown {
+	float: left;
+	margin: 5px 30px 15px 30px;
+	}
+
+div#rowscols {
+	float: right;
+	clear: right;
+	margin-bottom: 15px;
+	}
+
+img.mid {
+	margin-left: auto;
+	margin-right: auto;
+	clear: both;
+	text-align: center;
+	display: block;
+	}
+
+div#rotate {
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 5px;
+	margin-bottom: 15px;
+	text-align: center;
+	clear: right;
+	}
+
 /* Links to persons under a photo */
-.photohdr, .personlink	{
+div#personlink	{
 	text-align: center;
 	font-size: small;
+	width: 100%;
+	margin: 0 0 15px 0;
 	}
 
-.field, .fieldtitle, .fieldtitle-centered	{ 
-	font-size: medium; 
-	padding-left: 4px;
-	padding-right: 4px;
-	}
-
-.fieldtitle	{
-	text-align: right;
-	vertical-align: top;
-	font-weight: bold;
-	}
-	
 /* Text next to an input field, suggesting what to put there, such as "64 chars max" */
 .inputhint	{
 	font-size: small;
 	padding-left: 4px;
 	padding-right: 4px;
+	}
+
+span.inputhint	{
+	padding-left: 30px;
+	text-align: right;
 	}
 /* Checkbox on the annotate photo page */
 
@@ -398,21 +482,50 @@ td.prev, td.next { width: 20%; }
 	font-weight: bold;
 	}
 
-.editchoice	{
+div.editchoice	{
 	vertical-align: top;
 	font-size: small;
+	margin: 10px;
+	width: 10em;
+	position: absolute;
+	top: inherit;
 	}
+
 /* Thumbnail photo */
-.thumbnail	{
+div.thumbnail	{
 	text-align: center;
 	vertical-align: top;
 	width: <?php echo THUMB_SIZE ?>px;
+	float:left;
+	margin: 2px;
+	padding: 5px;
 	}
 
+fieldset.editphotos div.thumbnail {
+	z-index: 20;
+	display: inline;
+	float: right;
+	clear: none;	
+	}
+
+fieldset.editphotos-fields {
+	clear: none;
+	width: 100%;
+	padding-top: 10px;
+	}
+
+br	{
+	clear: both;
+	}
+	
 /* The random thumnail on the first page */
 #random.thumbnail	{
-	vertical-align: middle; 
-	padding: 10px;
+	width: <?php echo THUMB_SIZE+10 ?>px;
+	vertical-align: middle;
+	margin: 0;
+	padding-top: 10px;
+	padding-left: 0px;
+	float: left;
 	}
 
 /* Mid size photo */
@@ -424,13 +537,68 @@ td.prev, td.next { width: 20%; }
 .person, .place, .showattr {
 	text-align: left;
 	font-size: medium;
+	clear: left;
+	display: block;
 	}
 
 /* hr */
 .wide 	{
 	width: 90%
 	}
+.field, .fieldtitle, .fieldtitle-centered, label	{ 
+	font-size: medium; 
+	padding-left: 4px;
+	padding-right: 4px;
+	}
 
+.fieldtitle, label	{
+	text-align: right;
+	vertical-align: top;
+	font-weight: bold;
+	}
+
+label	{
+	margin-left: 1em;
+	margin-bottom: 10px;
+	width: 10em;
+	display: block;
+	float: left;
+	clear: none;
+	}
+
+/* This is to get the labels in a nice column, even if there's a checkbox
+next to it, like on the Annotate photo page */
+
+input[type="checkbox"] + label {
+	width: 9em;
+	}
+fieldset  {
+	margin: 0;
+	border: none;
+	padding: 0;
+	margin-bottom: 5px;
+	clear: right;
+	}
+
+fieldset.editphotos  {
+	margin-top: 10px;
+	margin-bottom: 5px;
+	padding-bottom: 20px;
+	border: 1px solid <?php echo $PAGE_BG_COLOR ?>;
+	height: inherit;
+	}
+
+fieldset.editphotos legend {
+	display: block;
+	position: relative;
+	left: 2em;
+	padding-right: 2em;
+	padding-left: 2em;
+        font-weight: bold;
+	border: 1px solid <?php echo $PAGE_BG_COLOR ?>;
+	background: <?php echo $TITLE_BG_COLOR ?>;
+	}
+	
 input[type="button"],
 input[type="submit"],
 input[type="reset"] {
@@ -440,7 +608,90 @@ input[type="reset"] {
         font-weight: bold;
         width: 100px;
         height: 25px;
+	margin-top: 15px;
+	margin-bottom: 15px;
 	margin-left: auto;
+	margin-right: 15px;
+	display: block;
+	}
+
+input[type="submit"].updatebutton {
+	clear: right;
+	margin-right: 15px;
+	margin-left: auto;
+	display: block;
+	}
+
+textarea {
+	margin: 2px;
+	}	
+input[type="checkbox"] {
+	float: left;
+	}
+
+div#logon input[type="text"],
+div#logon input[type="password"],
+div#passwordchange input[type="text"],
+div#passwordchange input[type="password"]	{
+	margin-bottom: 10px;
+	margin-left: 5px;
+	}
+
+input, select {
+	margin: 2px;
+	}
+
+table#users,
+table#credits, 
+table#user, 
+table#slideshow, 
+table#place,
+table#photo,
+table#editperson {
+	width: 100%;
+	}
+
+table.editphotos {
+	width: 350px;
+	}
+
+table#users td, 
+table#credits td, 
+table#user td,
+table#slideshow td,
+table#place td,
+table#photo td, 
+table#editperson td,
+table.editphotos td {
+	margin: 2px;
+	}
+
+table#zophinfo {
+	width: 60%;
+	margin-left: auto;
+	margin-right: auto;
+	}
+
+table#zophinfo td.fieldtitle {
+	width: 80%;
+	}
+
+table#zophinfo td.field {
+	width: 20%;
+	}
+	
+div#rowscols select,
+div#rowscols input,
+div#rotate select,
+div#rotate input,
+table#photo select,
+table#photo input {
+	margin: 0;
+	}
+
+div#rotate input[type="submit"] {
+	margin-left: auto;
+	margin-right: auto;
 	}
 
 /* This is a temporary style to workaround this bug: http://bugzilla.mozilla.org/show_bug.cgi?id=241317 */
@@ -475,4 +726,5 @@ input[type="reset"] {
 .version	{
 	text-align: center;
 	font-size: small;
+	margin-bottom: 2px;
 	}
