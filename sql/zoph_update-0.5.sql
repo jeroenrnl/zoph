6ee06c1b485a1dbf705dd60d3a017ef52249247a
jeroenrnl <jeroen@zoph.org>
2005-12-11 22:53:05 +0000

    Added upgrade script. This changes the length of the password field, for MySQL > 4.1 compatibility

---
+++ sql/zoph_update-0.5.sql
@@ -0,0 +1,26 @@
+#
+# Zoph 0.4 -> 0.5 update
+#
+# This file is part of Zoph.
+#
+# Zoph is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Zoph is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Zoph; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+# This will change the length of the password field to 64 bytes.
+# As of MySQL 4.1, the encrypted password size was too big for the old 32
+# bytes. Zoph will take care of the conversion from the old to the new
+# encryption method. When a user logs in for the first time since upgrading
+# to MySQL 4.1, Zoph updates the password field.
+
+ALTER TABLE zoph_users CHANGE password password varchar(64); 
