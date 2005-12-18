fb154255fa06d0e9b2b0d805fcb13fac6846aaea
jeroenrnl <jeroen@zoph.org>
2005-12-18 21:08:11 +0000

    Req#684986: Hierarchical locations. Crude implementation. May need some tinkering to improve usability.

--- php/place.inc.php
+++ php/place.inc.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-class place extends zoph_table {
+class place extends zoph_tree_table {
 
     function place($id = 0) {
         parent::zoph_table("places", array("place_id"), array("title"));
@@ -68,10 +68,9 @@ class place extends zoph_table {
         $link = "<a href=\"place.php?place_id=" . $this->get("place_id") . "\">" . $this->get_name() . "</a>";
 
         // add city link if title exists (and so was used by get_name())
-        if ($this->get("title") && $this->get("city")) {
-            $link .= ", <a href=\"places.php?_l=" . rawurlencode(strtolower($this->get("city"))) . "\">" . $this->get("city") . "</a>";
-        }
-
+  //      if ($this->get("title") && $this->get("city")) {
+    //        $link .= ", <a href=\"places.php?_l=" . rawurlencode(strtolower($this->get("city"))) . "\">" . $this->get("city") . "</a>";
+     //   }
         return $link;
     }
 
@@ -86,12 +85,100 @@ class place extends zoph_table {
             translate("country") => $this->get("country"),
             translate("notes") => $this->get("notes"));
     }
+    
+    function get_photo_count($user = null) {
+        if ($this->photo_count) { return $photo_count; }
+
+        $id = $this->get("place_id");
+
+        if ($user && !$user->is_admin()) {
+            $sql =
+                "select count(*) from " .
+                DB_PREFIX . "photo_albums as pa, " .
+                DB_PREFIX . "photos as p, " .
+                DB_PREFIX . "album_permissions as ap " .
+                "where p.location_id = $id" .
+                " and ap.user_id = '" . escape_string($user->get("user_id")) .
+                "' and ap.album_id = pa.album_id" .
+                " and pa.photo_id = p.photo_id " .
+                " and ap.access_level >= p.level";
+}
+        else {
+            $sql =
+                "select count(*) from " .
+                DB_PREFIX . "photos " .
+                "where location_id = '" .  escape_string($id) . "'";
+        }
+
+        return get_count_from_query($sql);
+    }
+
+    function get_total_photo_count($user = null) {
+        if ($this->get("parent_place_id")) {
+            $id_list = $this->get_branch_ids($user);
+            $id_constraint = "p.location_id in ($id_list)";
+        }
+        else {
+            $id_constraint = "";
+        }
+
+        if ($user && !$user->is_admin()) {
+            $sql =
+                "select count(distinct pa.photo_id) from " .
+                DB_PREFIX . "photo_albums as pa, " .
+                DB_PREFIX . "photos as p, " .
+                DB_PREFIX . "album_permissions as ap " .
+                "where ap.user_id = '" . escape_string($user->get("user_id")) .
+                "' and ap.album_id = pa.album_id " .
+                " and pa.photo_id = p.photo_id " .
+                " and ap.access_level >= p.level";
+
+            if ($id_constraint) {
+                $sql .= " and $id_constraint";
+            }
+        }
+        else {
+            $sql =
+                "select count(distinct p.photo_id) from " .
+                DB_PREFIX . "photos p ";
+                "where ($id_values)";
+
+            if ($id_constraint) {
+                $sql .= " where $id_constraint";
+            }
+        }
+
+        return get_count_from_query($sql);
+    }
+
 }
 
 function get_places($constraints = null, $conj = "and", $ops = null,
     $order = "city, title, address") {
 
     return get_records("place", $order, $constraints, $conj, $ops);
+}
+
+function get_children() {
+
+    $id = $this->get("album_id");
+    if (!$id) { return; }
+
+   $sql =
+         "select place_id, title, address, address2, city, ".
+         "state, zip, country, notes from " .
+         DB_PREFIX . "places" .
+         "where p.parent_place_id = '" . escape_string($id) . "'" .
+         " order by p.title";
+
+    $this->children = get_records_from_query("album", $sql);
+
+    return $this->children;
+
+}
+
+function get_root_place() {
+    return new place(1);
 }
 
 function get_photographed_places($user = null) {
@@ -123,25 +210,7 @@ function get_photographed_places($user = null) {
 }
 
 function get_places_select_array($user = null) {
-
-    $plc[""] = "";
-    if ($user) { // also used on search page
-        $places_array = get_photographed_places($user);
-    }
-    else {
-        $places_array = get_places();
-    }
-
-    if ($places_array) {
-        foreach ($places_array as $place) {
-            $c = $place->get("city");
-            if (!$c) { $c = "[No City]"; }
-            $t = $place->get("title");
-            $plc[$place->get("place_id")] = $t ? "$c, $t" : "$c";
-        }
-    }
-
-    return $plc;
+    return create_tree_select_array("place", $user, null, "", null, null);
 }
 
 function get_popular_places($user) {
