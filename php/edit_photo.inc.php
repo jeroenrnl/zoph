50248ee5d8758bea05448d4b71df8b3e030b3930
jeroenrnl <jeroen@zoph.org>
2005-12-28 13:40:27 +0000

    Req#1252457: Option to open the fullsize image in a new window.

--- php/edit_photo.inc.php
+++ php/edit_photo.inc.php
@@ -81,12 +81,12 @@
 
                 <div id="prev"><?php echo $prev_link ? "[ $prev_link ]" : "&nbsp;" ?></div>
                 <div id="photohdr">
-                  <?php echo $photo->get_fullsize_link($photo->get("name")) ?> :
+                  <?php echo $photo->get_fullsize_link($photo->get("name"),$FULLSIZE_NEW_WIN) ?> :
                   <?php echo $photo->get("width") ?> x <?php echo $photo->get("height") ?>,
                   <?php echo $photo->get("size") ?> <?php echo translate("bytes") ?>
                 </div>
                 <div id="next"><?php echo $next_link ? "[ $next_link ]" : "&nbsp;" ?></div>
-            <?php echo $photo->get_fullsize_link($photo->get_midsize_img()) ?>
+            <?php echo $photo->get_fullsize_link($photo->get_midsize_img(),$FULLSIZE_NEW_WIN) ?>
 <?php
     }
 ?>
