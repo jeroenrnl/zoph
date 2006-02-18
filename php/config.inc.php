717b6ee486c51a6731383c1d5630bd37c8ed2429
jeroenrnl <jeroen@zoph.org>
2006-02-18 20:15:16 +0000

    Updated version number/date for 0.5pre4

--- php/config.inc.php
+++ php/config.inc.php
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-    define('VERSION', '0.5pre3');
+    define('VERSION', '0.5pre4');
 
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'zoph');
