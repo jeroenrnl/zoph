e02e4ba85efc02615f53751d01bfcb1aba85e642
jeroenrnl <jeroen@zoph.org>
2006-06-04 18:54:38 +0000

    Bug#1500582: Object syntax in comment.inc.php not compatible with PHP4.

--- php/comment.inc.php
+++ php/comment.inc.php
@@ -28,39 +28,39 @@ class comment extends zoph_table {
     function insert() {
         $this->set("comment_date", "now()");
         $this->set("ipaddr", $_SERVER['REMOTE_ADDR']);
-	parent::insert();
-	$this->lookup();
+        parent::insert();
+        $this->lookup();
     }
     
     function update() {
         $this->set("timestamp","now()");
-	parent::update();
-	$this->lookup();
+        parent::update();
+        $this->lookup();
     }
 
     function delete() {
         if(!$this->get("comment_id")) { return; }
         parent::delete();
         
-	$sql = "delete from " . DB_PREFIX . "photo_comments where comment_id=";
-	$sql .= $this->get("comment_id");
-	
+        $sql = "delete from " . DB_PREFIX . "photo_comments where comment_id=";
+        $sql .= $this->get("comment_id");
+    
         mysql_query($sql) or die_with_mysql_error("Could not clean comment from photo: ", $sql);
     }
-	
-	
+    
+    
     function get_display_array($user = null) {
         $date=$this->get("comment_date");
-	$changed=$this->get("timestamp");
-	if($changed != $date) { $updated=$changed; }
-	
+        $changed=$this->get("timestamp");
+        if($changed != $date) { $updated=$changed; }
+    
         return array(
             translate("subject") => $this->get("subject"),
             translate("date") => $date,
             translate("user") => $this->lookup_user(),
             translate("IP address") => $user->is_admin() ? $this->get("ipaddr") : "<i>" . translate("only visible for admin users") . "</i>",
             translate("comment") => str_replace("\n", "<br>", $this->get("comment")),
-	    translate("updated") => $changed
+            translate("updated") => $changed
         );
     }
     
@@ -78,36 +78,37 @@ class comment extends zoph_table {
     function get_photo() {
         if(!$this->get("comment_id")) { return; }
         $sql = "select photo_id from " . DB_PREFIX . "photo_comments" .
-                  " where comment_id=" . $this->get("comment_id") .
-                  " limit 1";
+            " where comment_id=" . $this->get("comment_id") .
+            " limit 1";
         $result=get_records_from_query("photo", $sql);
         if($result[0]) { 
-	    $result[0]->lookup();
+            $result[0]->lookup();
             return $result[0];
-	} else {
-	    return null;
-	}
+        } else {
+            return null;
+        }
     }
 
     function add_comment_to_photo($photo_id) {
-       if (!$photo_id) { return; }
-       $sql = "insert into " . DB_PREFIX . "photo_comments values" . 
-                  "(" . $photo_id . ", " . $this->get("comment_id") . ")";
+        if (!$photo_id) { return; }
+        $sql = "insert into " . DB_PREFIX . "photo_comments values" . 
+            "(" . $photo_id . ", " . $this->get("comment_id") . ")";
 
       
-       mysql_query($sql) or die_with_mysql_error("Failed to add comment:", $sql);
+        mysql_query($sql) or die_with_mysql_error("Failed to add comment:", $sql);
     }
  
     function is_owner($user) {
         if($user->get(user_id)==$this->get("user_id")) {
-           return true;
+            return true;
         } else {
-           return false;
+            return false;
         }
     }
 
     function to_html($user, $thumbnail=null) {
         $this->lookup();
+        $photo=$this->get_photo();
 
         $html = "<div class=\"comment\">\n";
         $html .= "<h3>\n";
@@ -115,14 +116,14 @@ class comment extends zoph_table {
         if ($user->is_admin() || $this->is_owner($user)) {
             $html .= "<span class=\"actionlink\">\n";
             $html .= "<a href=\"comment.php?_action=display&amp;comment_id=" .
-                        $this->get("comment_id") . "\">\n";
+                $this->get("comment_id") . "\">\n";
             $html .= translate("display") . "</a> | ";
 
             $html .= "<a href=\"comment.php?_action=edit&amp;comment_id=" .
-                        $this->get("comment_id") . "\">";
+                $this->get("comment_id") . "\">";
             $html .= translate("edit") . "</a> | ";
             $html .= "<a href=\"comment.php?_action=delete&amp;comment_id=" .
-                         $this->get("comment_id") ."\">";
+                $this->get("comment_id") ."\">";
             $html .= translate("delete") . "</a>\n";
             $html .= "</span>\n";
         }
@@ -132,11 +133,13 @@ class comment extends zoph_table {
         $html .= $this->get("comment_date");
         $html .= " " . translate("by",0) . " <b>" . $this->lookup_user() . "</b>";
         $html .= "</div>\n";
-	if ($thumbnail) {
-	    $html .= "<div class=\"thumbnail\">\n";
-	    $html .= $this->get_photo()->get_thumbnail_link();
-	    $html .= "</div>\n";
-	}
+
+        if ($thumbnail) {
+            $html .= "<div class=\"thumbnail\">\n";
+            $html .= $photo()->get_thumbnail_link();
+            $html .= "</div>\n";
+        }
+        
         $html .= str_replace("\n", "<br class=\"noclear\">\n", $this->get("comment"));
         $html .= "<br></div>\n";
         return $html;
@@ -146,6 +149,4 @@ class comment extends zoph_table {
 function get_all_comments() {
    return get_records_from_query("comment", "select comment_id from " . DB_PREFIX . "comments");
    }
-
-P
 ?>
