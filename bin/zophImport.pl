8c63a26b67b66471695400832d93f188a1ab2b1d
jeroenrnl <jeroen@zoph.org>
2008-05-25 21:09:34 +0000

    Updating files for v0.7.0.4

--- bin/zophImport.pl
+++ bin/zophImport.pl
@@ -2,7 +2,7 @@
 
 #
 # zophImport.pl
-# Zoph 0.7
+# Zoph 0.7.0.4
 # Jason Geiger & Jeroen Roos, 2002-2008
 #
 # This file is part of Zoph.
@@ -54,7 +54,7 @@ use Cwd 'abs_path';
 
 $| = 1;
  
-my $version = '0.7.0.3';
+my $version = '0.7.0.4';
  
 
 my $update     = 0; # update existing photo records instead of inserting
