3c878aaea33d29face01c6c29f2533bf91ac12df
jeroenrnl <jeroen@zoph.org>
2008-12-22 22:59:54 +0000

    Changing version number to 0.7.4

--- php/config.inc.php
+++ php/config.inc.php
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-    define('VERSION', '0.7.3');
+    define('VERSION', '0.7.4');
 
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'zoph');
