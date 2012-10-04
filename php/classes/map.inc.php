fde1a1bd418d6d243bb4cf2f2bbf6614b79569d4
jeroenrnl <jeroen@zoph.org>
2012-10-04 21:37:09 +0200

    Documentation for map object
    Added phpdoc tags.

--- php/classes/map.inc.php
+++ php/classes/map.inc.php
@@ -38,6 +38,12 @@ class map extends block {
 
     protected $edit=false;
 
+    /**
+     * Create a new map object
+     * @param string template to use
+     * @param array variables to pass to template
+     * @return map new object
+     */
     function __construct($template="map", $vars=null) {
         if(!is_array($vars)) {
             $vars=array();
@@ -52,10 +58,19 @@ class map extends block {
 
     } 
 
+    /**
+     * Add a marker to the map
+     * @param marker marker to add
+     */
     public function addMarker(marker $marker) {
         $this->markers[]=$marker;
     }
 
+    /**
+     * Add multiple markers from objects
+     * @param array Array of objects to get markers from
+     * @param user Only get markers this user can see
+     */
     public function addMarkers(array $objs, user $user) {
         $markers=array();
         foreach($objs as $obj) {
@@ -66,6 +81,15 @@ class map extends block {
         }
     }
 
+    /**
+     * Get marker from object
+     * @param photo|place Object to get marker from
+     * @param user User, needed to generate quicklook
+     * @param string Icon to use
+     * @return marker created marker.
+     * @todo A "mapable" interface should be created to make sure
+             only certain objects can get passed to this function.
+     */
     public static function getMarkerFromObj($obj, user $user, $icon) {
         $lat=$obj->get("lat");
         $lon=$obj->get("lon");
@@ -88,28 +112,62 @@ class map extends block {
         return $markers;
     }
 
+    /**
+     * Checks whether this maps has markers
+     * @return bool
+     */
     public function hasMarkers() {
         return !empty($this->markers);
     }
 
+    /**
+     * Add a track
+     * @param track
+     */
     public function addTrack(track $track) {
         $this->tracks[]=$track;
     }
 
+    /**
+     * Get tracks
+     * @return Array tracks
+     */
     public function getTracks() {
         return $this->tracks;
     }
 
+    /**
+     * Checks whether this maps has tracks
+     * @return bool
+     */
     public function hasTracks() {
         return !empty($this->tracks);
     }
 
+    /**
+     * Set center and zoom
+     * This sets the center point and zoom level for the map
+     * @param float latitude
+     * @param float longitude
+     * @param int zoom level
+     */
     public function setCenterAndZoom($lat, $lon, $zoom) {
         $this->clat=(float) $lat;
         $this->clon=(float) $lon;
         $this->zoom=(int) $zoom;
     }
 
+    /**
+     * Set center and zoom from object
+     * Can take a location object and determine center and zoom from there
+     * it can also take a photo object to determine c&s.
+     * If a photo object does not have c&z, it will see if the photo has
+     * a location set, and determine it from there.
+     * If a location does not have c&z, it can go up in the tree until
+     * it find an ancestor with c&z set.
+     * @param photo|place object to get location from
+     * @todo mapable interface should be created
+     */
     public function setCenterAndZoomFromObj($obj) {
         $lat=$obj->get("lat");
         $lon=$obj->get("lon");
@@ -136,6 +194,11 @@ class map extends block {
         $this->setCenterAndZoom($lat, $lon, $zoom);
     }
 
+    /**
+     * Set whether or not this map can be changed
+     * (used to add markers to a place or photo)
+     * @param bool
+     */
     public function setEditable($edit=true) {
         $this->edit=(bool) $edit;
     }
