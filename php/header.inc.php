9859e8e6c184b6677df2ea851912cf067d2507e4
jeroenrnl <jeroen@zoph.org>
2013-07-01 12:30:27 +0200

    Whitespace fixes
    Cleanup of indentation.
    also added some phpdoc-headers

--- php/header.inc.php
+++ php/header.inc.php
@@ -1,5 +1,8 @@
 <?php
-/* This file is part of Zoph.
+/**
+ * Display the header of the page
+ * 
+ * This file is part of Zoph.
  *
  * Zoph is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,23 +16,27 @@
  * You should have received a copy of the GNU General Public License
  * along with Zoph; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package Zoph
+ * @author Jason Geiger
+ * @author Jeroen Roos
  */
 
-    header("Content-Type: text/html; charset=utf-8");
-    global $user;
+header("Content-Type: text/html; charset=utf-8");
+global $user;
 
-    $icons=array(
-        "count" => template::getImage("icons/photo.png"),
-        "taken" => template::getImage("icons/date.png"),
-        "modified" => template::getImage("icons/modified.png"),
-        "rated" => template::getImage("icons/rating.png"),
-        "children" => template::getImage("icons/folder.png"),
-        "geo-photo" => template::getImage("icons/geo-photo.png"),
-        "geo-place" => template::getImage("icons/geo-place.png"),
-        "resize" => template::getImage("icons/resize.png"), 
-        "unpack" => template::getImage("icons/unpack.png"),
-        "remove" => template::getImage("icons/remove.png"),
-        "down2" => template::getImage("down2.gif"),
+$icons=array(
+    "count" => template::getImage("icons/photo.png"),
+    "taken" => template::getImage("icons/date.png"),
+    "modified" => template::getImage("icons/modified.png"),
+    "rated" => template::getImage("icons/rating.png"),
+    "children" => template::getImage("icons/folder.png"),
+    "geo-photo" => template::getImage("icons/geo-photo.png"),
+    "geo-place" => template::getImage("icons/geo-place.png"),
+    "resize" => template::getImage("icons/resize.png"), 
+    "unpack" => template::getImage("icons/unpack.png"),
+    "remove" => template::getImage("icons/remove.png"),
+    "down2" => template::getImage("down2.gif"),
         "pleasewait" => template::getImage("pleasewait.gif")
    ); 
 ?>
@@ -52,138 +59,147 @@
         <script type="text/javascript" src="js/util.js"></script>
         <script type="text/javascript" src="js/xml.js"></script>
         <script type="text/javascript" src="js/thumbview.js"></script>
-<?php
-        if(basename($_SERVER["SCRIPT_NAME"])=="import.php") {
-?>
+        <?php
+if(basename($_SERVER["SCRIPT_NAME"])=="import.php") {
+    ?>
         <script type="text/javascript" src="js/import.js"></script>
-<?php
-        }
-        if(basename($_SERVER["SCRIPT_NAME"])=="config.php") {
-?>
+    <?php
+}
+if(basename($_SERVER["SCRIPT_NAME"])=="config.php") {
+    ?>
         <script type="text/javascript" src="js/conf.js"></script>
-<?php
-        }
-        if(conf::get("interface.autocomplete")) {
-?>
+    <?php
+}
+if(conf::get("interface.autocomplete")) {
+    ?>
         <script type="text/javascript" src="js/autocomplete.js"></script>
-<?php
-        }
-        if(conf::get("maps.provider")) {
-?>
-        <script type="text/javascript" src="js/mxn/mxn.js?(<?php echo conf::get("maps.provider"); ?>)"></script>
+    <?php
+}
+if(conf::get("maps.provider")) {
+    ?>
+        <script type="text/javascript" 
+            src="js/mxn/mxn.js?(<?php echo conf::get("maps.provider"); ?>)">
+        </script>
         <script type="text/javascript" src="js/maps.js"></script>
         <script type="text/javascript" src="js/custommaps.js"></script>
-<?php 
-        if(conf::get("maps.geocode")) { ?>
-
+    <?php
+    if(conf::get("maps.geocode")) { 
+        ?>
         <script type="text/javascript" src="js/geocode.js"></script>
-<?php
-        }        
-            switch (strtolower(conf::get("maps.provider"))) {
-            case 'googlev3':
-?>
-        <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-
-<?php
-            break;
-            case 'yahoo':
-?>
-        <script type="text/javascript" src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=Zoph"></script>
-<?php
-            break;
-            case 'openlayers':
-?>
-        <script src="http://openlayers.org/api/OpenLayers.js"></script>
-<?php
-            break;
-            case 'cloudmade':
-?>
-        <script type="text/javascript" src="http://tile.cloudmade.com/wml/0.2/web-maps-lite.js"></script>
+        <?php 
+    }        
+    switch (strtolower(conf::get("maps.provider"))) {
+    case 'googlev3':
+        ?>
+        <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript">
+        </script>
+        <?php
+        break;
+    case 'yahoo':
+        ?>
+        <script type="text/javascript" src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=Zoph">i
+        </script>
+        <?php
+        break;
+    case 'openlayers':
+        ?>
+        <script src="http://openlayers.org/api/OpenLayers.js">
+        </script>
+        <?php
+        break;
+    case 'cloudmade':
+        ?>
+        <script type="text/javascript" src="http://tile.cloudmade.com/wml/0.2/web-maps-lite.js">
+        </script>
         <script type="text/javascript">
             var cloudmade_key = "<?php echo conf::get("maps.key.cloudmade"); ?>";
         </script>
-<?php
-            break;
-            }
-        }
-    if (isset($extrastyle)) {
-?>
-        <style type="text/css">
-            <?php echo $extrastyle ?>
-        </style>
-<?php
+        <?php
+        break;
     }
-    $html_title=conf::get("interface.title");
-    if (isset($title)) {
-        $html_title.=" - " . $title;
-    }
+}
+if (isset($extrastyle)) {
+    ?>
+    <style type="text/css">
+        <?php echo $extrastyle ?>
+    </style>
+    <?php
+}
+$html_title=conf::get("interface.title");
+if (isset($title)) {
+    $html_title.=" - " . $title;
+}
 ?>
     <title><?php echo $html_title ?></title>
-    </head>
-    <body>
-        <ul class="menu">
+  </head>
+<body>
+  <ul class="menu">
 <?php
-    $tabs = array(
-        translate("home", 0) => "zoph.php",
-        translate("albums", 0) => "albums.php",
-        translate("categories", 0) => "categories.php");
-    if ($user->is_admin() || $user->get("browse_people")) {
-        $tabs[translate("people", 0)] = "people.php";
-    }
+$tabs = array(
+    translate("home", 0) => "zoph.php",
+    translate("albums", 0) => "albums.php",
+    translate("categories", 0) => "categories.php"
+);
 
-    if ($user->is_admin() || $user->get("browse_places")) {
-        $tabs[translate("places", 0)] = "places.php";
-    }
+if ($user->is_admin() || $user->get("browse_people")) {
+    $tabs[translate("people", 0)] = "people.php";
+}
 
-    $tabs[translate("photos", 0)] = "photos.php";
+if ($user->is_admin() || $user->get("browse_places")) {
+    $tabs[translate("places", 0)] = "places.php";
+}
 
-    if ($user->get("lightbox_id")) {
-        $tabs[translate("lightbox", 0)] = "photos.php?album_id=" .
-            $user->get("lightbox_id");
-    }
+$tabs[translate("photos", 0)] = "photos.php";
 
-    $tabs[translate("search",0)] = "search.php";
+if ($user->get("lightbox_id")) {
+    $tabs[translate("lightbox", 0)] = "photos.php?album_id=" .
+        $user->get("lightbox_id");
+}
 
-    if (conf::get("import.enable") &&
-        ($user->is_admin() || $user->get("import"))) {
+$tabs[translate("search",0)] = "search.php";
 
-        $tabs[translate("import", 0)] = "import.php";
-    }
+if (conf::get("import.enable") &&
+    ($user->is_admin() || $user->get("import"))) {
 
-    if ($user->is_admin()) {
-        $tabs[translate("admin", 0)] = "admin.php";
-    }
+    $tabs[translate("import", 0)] = "import.php";
+}
 
-    $tabs += array(
-        translate("reports", 0) => "reports.php",
-        translate("prefs", 0) => "prefs.php",
-        translate("about", 0) => "info.php");
+if ($user->is_admin()) {
+    $tabs[translate("admin", 0)] = "admin.php";
+}
 
-    if ($user->get("user_id") == conf::get("interface.user.default")) {
-        $tabs[translate("logon", 0)] = "zoph.php?_action=logout";
-    }
-    else {
-        $tabs[translate("logout", 0)] = "zoph.php?_action=logout";
-    }
+$tabs += array(
+    translate("reports", 0) => "reports.php",
+    translate("prefs", 0) => "prefs.php",
+    translate("about", 0) => "info.php"
+);
 
-    if (strpos($_SERVER["PHP_SELF"], "/") === false) {
-        $self = $_SERVER["PHP_SELF"];
-    }
-    else {
-        $self = substr(strrchr($_SERVER['PHP_SELF'], "/"), 1);
-    }
+if ($user->get("user_id") == conf::get("interface.user.default")) {
+    $tabs[translate("logon", 0)] = "zoph.php?_action=logout";
+} else {
+    $tabs[translate("logout", 0)] = "zoph.php?_action=logout";
+}
 
-    while (list($label, $page) = each($tabs)) {
-        if ($page == $self) {
-            $class = "class=\"selected\"";
-        } else {
-    $class="";
+if (strpos($_SERVER["PHP_SELF"], "/") === false) {
+    $self = $_SERVER["PHP_SELF"];
+} else {
+    $self = substr(strrchr($_SERVER['PHP_SELF'], "/"), 1);
+}
+
+while (list($label, $page) = each($tabs)) {
+    if ($page == $self) {
+        $class = "class=\"selected\"";
+    } else {
+        $class="";
     }
-?><li <?php echo $class ?>><a href="<?php echo $page ?>"><?php echo $label ?></a></li><?php
-    }
-    echo "\n";
+    ?>
+    <li <?php echo $class ?>>
+        <a href="<?php echo $page ?>"><?php echo $label ?></a>
+    </li>
+    <?php
+}
 ?>
-    </ul>
+</ul>
 <?php
 require_once "breadcrumbs.inc.php";
 ?>
--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,7 @@
 <?php
-/*
+/**
+ * Mail a photo
+ * 
  * This file is part of Zoph.
  *
  * Zoph is free software; you can redistribute it and/or modify
@@ -14,209 +16,230 @@
  * You should have received a copy of the GNU General Public License
  * along with Zoph; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package Zoph
+ * @author Jason Geiger
+ * @author Jeroen Roos
  */
-    require_once "include.inc.php";
 
-    $title = translate("E-Mail Photo");
+require_once "include.inc.php";
 
-    $photo_id = getvar("photo_id");
-    $html = getvar("html");
-    $to_name = getvar("to_name");
-    $to_email = getvar("to_email");
-    $from_name = getvar("from_name");
-    $from_email = getvar("from_email");
-    $subject = getvar("subject");
-    $message = getvar("message");
-    $includeurl = getvar("includeurl");
+$title = translate("E-Mail Photo");
 
-    if (conf::get("feature.annotate")) {
-        $annotate = getvar("annotate");
-        $annotate_vars = getvar("annotate_vars");
+$photo_id = getvar("photo_id");
+$html = getvar("html");
+$to_name = getvar("to_name");
+$to_email = getvar("to_email");
+$from_name = getvar("from_name");
+$from_email = getvar("from_email");
+$subject = getvar("subject");
+$message = getvar("message");
+$includeurl = getvar("includeurl");
+
+if (conf::get("feature.annotate")) {
+    $annotate = getvar("annotate");
+    $annotate_vars = getvar("annotate_vars");
+} else {
+    $annotate=0;
+}
+
+if ($annotate) {
+    $skipcrumb = true;
+    $photo=new annotatedPhoto($photo_id);
+    if(!empty($annotate_vars)) {
+        parse_str($annotate_vars, $vars);
     } else {
-        $annotate=0;
+        $vars=$request_vars;
+        $annotate_vars=http_build_query($vars, "&amp;");
     }
+    $photo->setVars($vars);
+} else {
+    $photo = new photo($photo_id);
+}
 
-    if ($annotate) {
-        $skipcrumb = true;
-        $photo=new annotatedPhoto($photo_id);
-        if(!empty($annotate_vars)) {
-            parse_str($annotate_vars, $vars);
-        } else {
-            $vars=$request_vars;
-            $annotate_vars=http_build_query($vars, "&amp;");
-        }
-        $photo->setVars($vars);
-    } else {
-        $photo = new photo($photo_id);
-    }
+$found = $photo->lookup();
 
-    $found = $photo->lookup();
+if (!$found) {
+    $msg = sprintf(translate("Could not find photo id %s."), $photo_id);
+}
+else {
 
-    if (!$found) {
-        $msg = sprintf(translate("Could not find photo id %s."), $photo_id);
-    }
-    else {
+    if ($_action == "mail") {
+        try {
+            $mail = new mailMime();
+            $hdrs = array (
+                "X-Mailer" => "Html Mime Mail Class",
+                "X-Zoph-Version" => VERSION
+            );
+            $headers="";
+            
+            $size = getvar("_size");
+            
+            if($annotate) {
+                $file=$photo->get("name");
+                $size = $vars["_size"];
+            } else if ($size == "full") {
+                $file = $photo->get("name");
+                $dir = conf::get("path.images") . "/" . $photo->get("path") . "/";
+            } else {
+                $file = MID_PREFIX . "_" . $photo->get("name");
+                $dir = conf::get("path.images") . "/" . $photo->get("path") . "/" .
+                    MID_PREFIX . "/";
+            }
 
-        if ($_action == "mail") {
-            try {
-                $mail = new mailMime();
-                $hdrs = array (
-                    "X-Mailer" => "Html Mime Mail Class",
-                    "X-Zoph-Version" => VERSION
-                );
-                $headers="";
-                
-                $size = getvar("_size");
+            if ($html) {
+                $html = "<center>\n"; 
+                $html .= "<img src=\"" . $file . "\"><br>\n";
+                $html .= str_replace("\n", "<br>\n", $message);
+                if($includeurl) {
+                    $html .= "<a href=\"" . getZophURL() . 
+                        "/photo.php?photo_id=" . $photo_id . "\">";
+                    $html .= sprintf(translate("See this photo in %s"), 
+                        conf::get("interface.title"));
+                    $html .= "</a>";
+                }
+                $html .= "</center>\n";
                 
                 if($annotate) {
-                    $file=$photo->get("name");
-                    $size = $vars["_size"];
-                } else if ($size == "full") {
-                    $file = $photo->get("name");
-                    $dir = conf::get("path.images") . "/" . $photo->get("path") . "/";
+                    list($headers,$image)=$photo->display($size);
+                    $mail->addHTMLImageFromString($image, $photo->get("name"), 
+                        $headers["Content-type"]);
                 } else {
-                    $file = MID_PREFIX . "_" . $photo->get("name");
-                    $dir = conf::get("path.images") . "/" . $photo->get("path") . "/" .
-                        MID_PREFIX . "/";
+                    $mail->addHTMLImageFromFile($dir . "/" . $file, get_image_type($file));
                 }
-
-                if ($html) {
-                    $html = "<center>\n"; 
-                    $html .= "<img src=\"" . $file . "\"><br>\n";
-                    $html .= str_replace("\n", "<br>\n", $message);
-                    if($includeurl) {
-                        $html .= "<a href=\"" . getZophURL() . "/photo.php?photo_id=" . $photo_id . "\">" . sprintf(translate("See this photo in %s"), conf::get("interface.title")) . "</a>";
-                    }
-                    $html .= "</center>\n";
-                    
-                    if($annotate) {
-                        list($headers,$image)=$photo->display($size);
-                        $mail->addHTMLImageFromString($image, $photo->get("name"), $headers["Content-type"]);
-                    } else {
-                        $mail->addHTMLImageFromFile($dir . "/" . $file, get_image_type($file));
-                    }
-                    $mail->setHTMLBody($html);
-                    $mail->setTXTBody($message);
+                $mail->setHTMLBody($html);
+                $mail->setTXTBody($message);
+            } else {
+                if($includeurl) {
+                    $message .= "\n"
+                    $message .= sprintf(translate("See this photo in %s"), 
+                        conf::get("interface.title"));
+                    $message .= ": " . getZophURL() . "/photo.php?photo_id=" . $photo_id;
+                }
+                $mail->setTXTBody($message);
+                if($annotate) {
+                    list($headers,$image)=$photo->display($size);
+                    $mail->addAttachmentFromString($image, $photo->get("name"), 
+                        $headers["Content-type"]);
                 } else {
-                    if($includeurl) {
-                        $message .= "\n" . sprintf(translate("See this photo in %s"), conf::get("interface.title")) . ": " . getZophURL() . "/photo.php?photo_id=" . $photo_id;
-                    }
-                    $mail->setTXTBody($message);
-                    if($annotate) {
-                        list($headers,$image)=$photo->display($size);
-                        $mail->addAttachmentFromString($image, $photo->get("name"), $headers["Content-type"]);
-                    } else {
-                        $mail->addAttachmentFromFile($dir . "/" . $file, get_image_type($file));
-                    }
+                    $mail->addAttachmentFromFile($dir . "/" . $file, get_image_type($file));
                 }
-                $mail->setFrom("$from_name <$from_email>");
-
-                if (strlen(conf::get("feature.mail.bcc")) > 0) {
-                    $mail->addBcc(conf::get("feature.mail.bcc"));
-                }
-                $body = $mail->get();
-                $hdrs = $mail->headers($hdrs);
-                foreach($hdrs as $header => $content) {
-                    $headers .= $header . ": " . $content . "\n";
-                }
-                if (mail($to_email,$subject, $body,$headers)) {
-                    $msg = translate("Your mail has been sent.");
-                } else {
-                    $msg = translate("Could not send mail.");
-                }
-            } catch (MailException $e) {
-                $msg = $e->getMessage();
             }
-        }
+            $mail->setFrom("$from_name <$from_email>");
 
+            if (strlen(conf::get("feature.mail.bcc")) > 0) {
+                $mail->addBcc(conf::get("feature.mail.bcc"));
+            }
+            $body = $mail->get();
+            $hdrs = $mail->headers($hdrs);
+            foreach($hdrs as $header => $content) {
+                $headers .= $header . ": " . $content . "\n";
+            }
+            if (mail($to_email,$subject, $body,$headers)) {
+                $msg = translate("Your mail has been sent.");
+            } else {
+                $msg = translate("Could not send mail.");
+            }
+        } catch (MailException $e) {
+            $msg = $e->getMessage();
+        }
     }
 
-    $from_name = $user->person->getName();
-    $from_email = $user->person->getEmail();
+}
 
-    require_once "header.inc.php";
+$from_name = $user->person->getName();
+$from_email = $user->person->getEmail();
+
+require_once "header.inc.php";
 ?>
 
-          <h1>
-  <?php if (conf::get("feature.annotate")) {
+      <h1>
+<?php 
+if (conf::get("feature.annotate")) {
+    ?>
+      <span class="actionlink"> 
+        <a href="define_annotated_photo.php?photo_id=<?php echo $photo->get("photo_id") ?>">
+          <?php echo translate("create annotated photo", 0) ?>
+        </a> 
+      </span>
+    <?php
+    }
 ?>
-          <span class="actionlink"> 
-            <a href="define_annotated_photo.php?photo_id=<?php echo $photo->get("photo_id") ?>"><?php echo translate("create annotated photo", 0) ?></a> 
-          </span>
-<?php
-        }
-?>
-  <?php echo translate("email photo") ?>
-          </h1>
+<?php echo translate("email photo") ?>
+      </h1>
 <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
-      <div class="main">
+  <div class="main">
 <?php
-    if (isset($msg)) {
-?>
-            <?php echo $msg ?>
-<?php
-    }
+if (isset($msg)) {
+    echo $msg;
+}
 
-    if ($found && $_action == "compose") {
-        $body="";
+if ($found && $_action == "compose") {
+    $body="";
 
-        $subject = sprintf(translate("A Photo from %s"), conf::get("interface.title")) . ": " . $photo->get("name");
-        $ea = $photo->getEmailArray();
+    $subject = sprintf(translate("A Photo from %s"), conf::get("interface.title")) . ": ";
+    $subject.= $photo->get("name");
+    $ea = $photo->getEmailArray();
 
-        if ($ea) {
-            while (list($name, $value) = each($ea)) {
-                if ($name && $value) {
-                    $body .= "$name: $value\r\n";
-                }
+    if ($ea) {
+        while (list($name, $value) = each($ea)) {
+            if ($name && $value) {
+                $body .= "$name: $value\r\n";
             }
         }
-?>
-<input type="hidden" name="_action" value="mail">
-<input type="hidden" name="photo_id" value="<?php echo $photo_id ?>">
-<input type="hidden" name="annotate" value="<?php echo $annotate ?>">
-       <label for="html"><?php echo translate("send as html") ?></label>
-       <?php echo template::createYesNoPulldown("html", "1") ?><br>
-       <label for="toname"><?php echo translate("to (name)") ?></label>
-       <?php echo create_text_input("to_name", $to_name, 24, 32) ?><br>
-       <label for="toemail"><?php echo translate("to (email)") ?></label>
-       <?php echo create_text_input("to_email", $to_email, 24, 32) ?><br>
-       <label for="fromname"><?php echo translate("from (your name)") ?></label>
-       <?php echo create_text_input("from_name", $from_name, 24, 32) ?><br>
-       <label for="fromemail"><?php echo translate("from (your email)") ?></label>
-       <?php echo create_text_input("from_email", $from_email, 24, 64) ?><br>
-       <label for="subject"><?php echo translate("subject") ?></label>
-       <?php echo create_text_input("subject", $subject, 48, 64) ?><br>
-<?php
-        if (!$annotate) {
-?>
-           <label for="size"><?php echo translate("send fullsize") ?></label>
-           <?php echo template::createPulldown("_size", "mid", array("full" => translate("Yes",0), "mid" => translate("No",0)) ) ?><br>
-           <label for="includeurl"><?php echo translate("include URL") ?></label>
-           <?php echo template::createYesNoPulldown("includeurl", "1") ?><br>
-<?php
-        }
-?>
-            <label for="message"><?php echo translate("message:") ?></label><br>
-            <textarea name="message" class="email" cols="70" rows="5"><?php echo $body ?></textarea>
-<?php
-        if ($annotate) {
-?>
-            <input type="hidden" name="annotate_vars" value="<?php echo $annotate_vars ?>">
-            <img src="image.php?photo_id=<?php echo $photo_id ?>&annotated=1&<?php echo $annotate_vars ?>" alt="<?= $photo->get("title") ? $photo->get("title") : $photo->get("name") ?>">
-<?php
-        } else {
-?>
-            <?php echo $photo->getImageTag(MID_PREFIX) ?>
-<?php
-        }
-?>
-            <input type="submit" name="_button" value="<?php echo translate("email", 0); ?>">
-<?php
     }
+    ?>
+    <input type="hidden" name="_action" value="mail">
+    <input type="hidden" name="photo_id" value="<?php echo $photo_id ?>">
+    <input type="hidden" name="annotate" value="<?php echo $annotate ?>">
+    <label for="html"><?php echo translate("send as html") ?></label>
+    <?php echo template::createYesNoPulldown("html", "1") ?><br>
+    <label for="toname"><?php echo translate("to (name)") ?></label>
+    <?php echo create_text_input("to_name", $to_name, 24, 32) ?><br>
+    <label for="toemail"><?php echo translate("to (email)") ?></label>
+    <?php echo create_text_input("to_email", $to_email, 24, 32) ?><br>
+    <label for="fromname"><?php echo translate("from (your name)") ?></label>
+    <?php echo create_text_input("from_name", $from_name, 24, 32) ?><br>
+    <label for="fromemail"><?php echo translate("from (your email)") ?></label>
+    <?php echo create_text_input("from_email", $from_email, 24, 64) ?><br>
+    <label for="subject"><?php echo translate("subject") ?></label>
+    <?php echo create_text_input("subject", $subject, 48, 64) ?><br>
+    <?php
+    if (!$annotate) {
+        ?>
+        <label for="size"><?php echo translate("send fullsize") ?></label>
+        <?php echo template::createPulldown("_size", "mid", array(
+            "full" => translate("Yes",0), 
+            "mid" => translate("No",0)) ); ?>
+        <br>
+        <label for="includeurl"><?php echo translate("include URL") ?></label>
+        <?php echo template::createYesNoPulldown("includeurl", "1") ?><br>
+        <?php   
+    }
+    ?>
+    <label for="message"><?php echo translate("message:") ?></label><br>
+    <textarea name="message" class="email" cols="70" rows="5"><?php echo $body ?></textarea>
+    <?php
+    if ($annotate) {
+        ?>
+        <input type="hidden" name="annotate_vars" value="<?php echo $annotate_vars ?>">
+        <img src="image.php?photo_id=<?php echo $photo_id 
+           ?>&annotated=1&<?php echo $annotate_vars ?>"
+           alt="<?php echo $photo->get("title") ? $photo->get("title") : $photo->get("name") ?>">
+        <?php
+    } else {
+        ?>
+        <?php echo $photo->getImageTag(MID_PREFIX) ?>
+        <?php
+    }
+    ?>
+    <input type="submit" name="_button" value="<?php echo translate("email", 0); ?>">
+    <?php
+}
 ?>
-      </div>
+  </div>
 </form>
 
 <?php
-    require_once "footer.inc.php";
+require_once "footer.inc.php";
 ?>
