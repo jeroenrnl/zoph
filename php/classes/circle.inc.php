7e58d0491e6cc7bf2aa1f41c56fc9f1f443b95d7
jeroenrnl <jeroen@zoph.org>
2016-02-08 22:49:05 +0100

    Hide people with no photos in Circle view
    In 'classic' people view, people that were not on a photo and had not
    taken a photo - in either case that means: a photo the user is allowed
    to see, these people were hidden for that user as well.
    
    Until now, those people were visible in the 'circles' view. This has now
    changed and the behaviour is consistent.
    
    Issue #46

--- php/classes/circle.inc.php
+++ php/classes/circle.inc.php
@@ -153,6 +153,17 @@ class circle extends zophTable {
         $qry->where(new clause("circle_id=:circleid"));
         $qry->addParam(new param(":circleid", (int) $this->getId(), PDO::PARAM_INT));
 
+        if(!user::getCurrent()->isAdmin()) {
+            $allowed=person::getAllPeopleAndPhotoGraphers();
+            $ids=array();
+            foreach($allowed as $person) {
+                $ids[]=$person->getId();
+            }
+            $param=new param(":peopledIds", $ids, PDO::PARAM_INT);
+            $qry->addParam($param);
+            $qry->addClause(clause::InClause("person_id", $param, "AND"));
+        }
+
         return person::getRecordsFromQuery($qry);
     }
 
